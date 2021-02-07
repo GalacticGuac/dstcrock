e799ad09b06d0b731696eea8d80c366de1eb5e3e
Brian Douglas <bvindouglas@gmail.com>
2021-02-06 22:25:43 -0800

    Fixed formatting

--- src/cook.go
+++ src/cook.go
@@ -8,59 +8,63 @@ import (
 )
 
 type IngrediantList struct {
-	Ingredient map[string]IngredientDetails 
+	Ingredient map[string]IngredientDetails
 }
 
 type IngredientDetails struct {
-	INGREDIENT string  `json:"INGREDIENT"`
-	MEAT       float64 `json:"MEAT"`
-	FISH       float64 `json:"FISH"`
-	EGG        float64 `json:"EGG"`
-	FRUIT      float64 `json:"FRUIT"`
-	VEGETABLE  float64 `json:"VEGETABLE"`
-	SWEETENER  float64 `json:"SWEETENER"`
-	MONSTER    float64 `json:"MONSTER"`
-	DAIRY      float64 `json:"DAIRY"`
-	BUG        float64 `json:"BUG"`
-	INEDIBLE   float64 `json:"INEDIBLE"`
-	MISC       float64 `json:"MISC"`
-	HEALTH     float64 `json:"HEALTH"`
-	HUNGER     float64 `json:"HUNGER"`
-	SANITY     float64 `json:"SANITY"`
-	DAYS       float64 `json:"DAYS"`
-	NOTES      string  `json:"NOTES"`
+	NAME      string  `json:"INGREDIENT"`
+	MEAT      float64 `json:"MEAT"`
+	FISH      float64 `json:"FISH"`
+	EGG       float64 `json:"EGG"`
+	FRUIT     float64 `json:"FRUIT"`
+	VEGETABLE float64 `json:"VEGETABLE"`
+	SWEETENER float64 `json:"SWEETENER"`
+	MONSTER   float64 `json:"MONSTER"`
+	DAIRY     float64 `json:"DAIRY"`
+	BUG       float64 `json:"BUG"`
+	INEDIBLE  float64 `json:"INEDIBLE"`
+	MISC      float64 `json:"MISC"`
+	HEALTH    float64 `json:"HEALTH"`
+	HUNGER    float64 `json:"HUNGER"`
+	SANITY    float64 `json:"SANITY"`
+	DAYS      float64 `json:"DAYS"`
+	NOTES     string  `json:"NOTES"`
 }
 
 func main() {
 
-	jsonFile, err := os.Open("../lib/ingredients.json")
+	jsonFile, err := os.Open("../lib/ingredients-reformat.json")
 	if err != nil {
 		fmt.Println(err)
 	}
-	
+
 	byteValue, err := ioutil.ReadAll(jsonFile)
 	if err != nil {
 		fmt.Println(err)
 	}
-	var ingredientsData []IngredientDetails
+	ingredientsData := map[string]IngredientDetails{}
 	err = json.Unmarshal(byteValue, &ingredientsData)
 	if err != nil {
 		fmt.Println(err)
 	}
-	// 0 3 7 10
 
+	// Set up fake Data/user input data
 	ingredients := []IngredientDetails{}
-	ingredients = append(ingredients, ingredientsData[0])
-	ingredients = append(ingredients, ingredientsData[3])
-	ingredients = append(ingredients, ingredientsData[7])
-	ingredients = append(ingredients, ingredientsData[10])
+	ingredients = append(ingredients, ingredientsData["Morsel"])
+	ingredients = append(ingredients, ingredientsData["Egg"])
+	ingredients = append(ingredients, ingredientsData["Egg"])
+	ingredients = append(ingredients, ingredientsData["Monster Meat"])
 
-	// fmt.Println(ingredients)
 	meatVal := 0.0
 	vegVal := 0.0
 	fruitVal := 0.0
 	eggVal := 0.0
+
+	titles := []string{}
+
 	for _, ingre := range ingredients {
+		titles = append(titles, ingre.NAME)
+
 		meatVal += ingre.MEAT
 
 		vegVal += ingre.VEGETABLE
@@ -70,9 +74,13 @@ func main() {
 		eggVal += ingre.EGG
 
 	}
-	fmt.Println("meat", meatVal)
-	fmt.Println("vegetable", vegVal)
-	fmt.Println("fruit", fruitVal)
-	fmt.Println("egg", eggVal)
+
+	title := fmt.Sprintf("%s + %s + %s + %s:", titles[0], titles[1], titles[2], titles[3])
+	fmt.Println(title)
+
+	fmt.Println("Meat: ", meatVal)
+	fmt.Println("Vegetable: ", vegVal)
+	fmt.Println("Fruit: ", fruitVal)
+	fmt.Println("Egg: ", eggVal)
 
 }

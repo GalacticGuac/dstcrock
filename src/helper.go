eea1080cf1fdf65dcf98bd7d44f176ea9861e96d
aznbio123 <sidsya4@yahoo.com>
2021-03-13 14:55:08 -0500

    03/13/2021 2:54pm

--- src/helper.go
+++ src/helper.go
@@ -21,6 +21,7 @@ func stringNotInSlice(a string, x []IngredientDetails) bool {
 }
 
 // count strings in cropost array
+
 func countIngName(x string, crockPot []IngredientDetails) int {
 	count := 0
 	for _, cp := range crockPot {
@@ -171,5 +172,4 @@ func countIngName(x string, crockPot []IngredientDetails) int {
 // }
 // if strings.Contains(x, "x") {
 // 	fmt.Println("Crock Has Exclusion")
-// }
 // }
